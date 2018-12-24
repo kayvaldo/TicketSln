2ab698ce7d763d0d061b65f269e2043991084c79
kayvaldo <kayvaldo@yahoo.co.uk>
2018-12-24 11:31:48 +0100

    Add files via upload

---
+++ NRC-WEB/Scripts/booking.js
@@ -0,0 +1,84 @@
+﻿$(function ()
+{
+    $('#bookingForm').hide();
+    SetAjaxImageMargins();
+})
+
+function ShowLoading()
+{
+    $('#ajax').modal();
+
+}
+
+function EndLoading()
+{
+    $('#ajax').modal('hide');
+
+}
+
+function ShowBookingForm()
+{
+    $('#bookingForm').show();
+
+}
+
+function SetAjaxImageMargins()
+{
+    //set ajax image margins
+    var windowHeight = Number($(window).height());
+    var windowWidth = Number($(window).width());
+    var imageheight = Number($("#ajax-image").attr("height"));
+    var imagewidth = Number($("#ajax-image").attr("width"));
+    var newHeight = ((windowHeight - imageheight) / 2);
+    var newWidth = ((windowWidth - imagewidth) / 2);
+    $("#ajax-image").css("margin-top", newHeight);
+    $("#ajax-image").css("margin-left", newWidth);
+}
+
+function SetTicketMax(count)
+{
+    var max = Number(count);
+    $('#numberOfTickets').prop('max', count);
+    //alert(count);
+}
+
+function ValidateForm()
+{
+    var selectedCheckboxes = $("input:checked").length;
+    if (selectedCheckboxes < 1)
+    {
+        alert("Please select a trip from the provided options.")
+        return false;
+    }
+    else
+    {
+        $("#submitBtn").trigger("click");
+    }
+
+}
+
+
+function SetCountRange(checkboxId)
+{
+    var checkbox = document.getElementById(checkboxId);
+    var textBoxId = $(checkbox).attr('textboxId');
+    var textBox = document.getElementById(textBoxId);
+
+    //Set required && min
+    if ($(checkbox).prop('checked'))
+    {
+        $(textBox).attr('required', 'required');
+        $(textBox).attr('min', 1);
+        $(textBox).attr('disabled', false);
+        $(textBox).val(1);
+    } else
+    {
+        $(textBox).removeAttr('required');
+        $(textBox).removeAttr('min');
+        $(textBox).attr('disabled', true);
+        $(textBox).val(0);
+    }
+
+    //alert('this.checked: ' + $(checkbox).prop('checked'));
+  
+}
---
+++ NRC-WEB/Scripts/schedules.js
@@ -0,0 +1,144 @@
+﻿$(function ()
+{
+
+    InitializeMultiSelectListBox();
+    SetAjaxImageMargins();
+    SetSelectedScheduleDays();
+    // ShowSelectedTrainTypeInfo();
+
+});
+
+
+function ShowSelectedTrainTypeInfo()
+{
+    //Set selected Schedule days
+    if ($('#selectedItems')) // I use this to check if page is Edit
+    {
+        var selected = $('#TrainTypeId').val();
+        var code = { trainTypeId: selected };
+        $('#ajax').modal('show');
+
+        $.ajax({
+            type: 'GET',
+            url: '/TrainTypes/TrainTypeTravelClassInfo',
+            dataType: 'html',
+            data: code,
+            success: function (result)
+            {
+                //hide loading modal
+                // $('#ajax').modal('hide');
+                $('#trainTypeInfoDiv').html(result);
+                $('#ajax').modal('hide');
+
+            },
+            error: function (result)
+            {
+                //$('#loadingImage').hide(); 
+                $('#ajax').modal('hide');
+                //ShowMessage('Error occured.The selected transaction(s) could NOT be approved.', 'error', 'Error');
+            },
+
+        });
+    }
+
+}
+
+
+function GetStations(element)
+{
+    var cityId = $(element).val();
+    var code = { cityId: cityId };
+    var stationsElement = document.getElementById($(element).attr('stationsElement'));
+    $('#ajax').modal('show');
+
+    $.ajax({
+        type: 'GET',
+        url: '/Stations/GetStationsByCity',
+        dataType: 'html',
+        data: code,
+        success: function (result)
+        {
+            //hide loading modal
+            // $('#ajax').modal('hide');
+            $(stationsElement).html(result);
+            $('#ajax').modal('hide');
+
+        },
+        error: function (result)
+        {
+            //$('#loadingImage').hide(); 
+            $('#ajax').modal('hide');
+            //ShowMessage('Error occured.The selected transaction(s) could NOT be approved.', 'error', 'Error');
+        },
+
+    });
+}
+
+function SetSelectedScheduleDays()
+{
+    //Set selected Schedule days
+    if ($('#selectedItems'))
+    {
+        var data = $('#selectedItems').val();
+
+        //Make an array
+        var dataarray = data.split(",");
+
+        // Set the value
+        $("#DaysOfWeek").val(dataarray);
+
+        // Then refresh
+        $("#DaysOfWeek").multiselect("refresh");
+    }
+}
+
+function InitializeMultiSelectListBox()
+{
+    ////Initialize multiselect listbox
+    $('.listbox').multiselect({
+        includeSelectAllOption: true
+    });
+}
+
+function SetAjaxImageMargins()
+{
+    //set ajax image margins
+    var windowHeight = Number($(window).height());
+    var windowWidth = Number($(window).width());
+    var imageheight = Number($("#ajax-image").attr("height"));
+    var imagewidth = Number($("#ajax-image").attr("width"));
+    var newHeight = ((windowHeight - imageheight) / 2);
+    var newWidth = ((windowWidth - imagewidth) / 2);
+    $("#ajax-image").css("margin-top", newHeight);
+    $("#ajax-image").css("margin-left", newWidth);
+}
+
+function GetTrainTypeInfo()
+{
+    var selected = $('#TrainTypeId').val();
+    var code = { trainTypeId: selected };
+    $('#ajax').modal('show');
+
+    $.ajax({
+        type: 'GET',
+        url: '/TrainTypes/TrainTypeTravelClassInfo',
+        dataType: 'html',
+        data: code,
+        success: function (result)
+        {
+            //hide loading modal
+            // $('#ajax').modal('hide');
+            $('#trainTypeInfoDiv').html(result);
+            $('#ajax').modal('hide');
+
+        },
+        error: function (result)
+        {
+            //$('#loadingImage').hide(); 
+            $('#ajax').modal('hide');
+            //ShowMessage('Error occured.The selected transaction(s) could NOT be approved.', 'error', 'Error');
+        },
+
+    });
+
+}
